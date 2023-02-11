804e1ef88ffae5d34465bd707e9041a892d24319
akirou-lab <>
2023-02-11 05:47:59 -0300

    0.1.6

--- dist/node/krop.cjs
+++ dist/node/krop.cjs
@@ -1,4 +1,4 @@
-// Krop v0.1.5 Copyright (c) 2023 Kori <korinamez@gmail.com> and contributors
+// Krop v0.1.6 Copyright (c) 2023 Kori <korinamez@gmail.com> and contributors
 'use strict';
 
 const http = require('http');
