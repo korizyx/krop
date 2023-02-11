8983b0a473cf8b4b3c1fd50a74374dee9de29dc1
akirou-lab <>
2023-02-11 05:44:54 -0300

    0.1.5

--- dist/krop.js
+++ dist/krop.js
@@ -1,4 +1,4 @@
-// Hermes v0.1.4 Copyright (c) 2023 Kori <korinamez@gmail.com> and contributors
+// Krop v0.1.5 Copyright (c) 2023 Kori <korinamez@gmail.com> and contributors
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory(require('http'), require('https'), require('http2'), require('assert')) :
   typeof define === 'function' && define.amd ? define(['http', 'https', 'http2', 'assert'], factory) :
